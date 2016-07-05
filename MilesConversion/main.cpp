cc58626f16b2f07050f40c8254ba8fa1717de101
Brent Perry <burentoperii@gmail.com>
2016-07-05 17:22:47 -0500

    Initial commit of MilesConversion.

--- MilesConversion/main.cpp
+++ MilesConversion/main.cpp
@@ -6,10 +6,18 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+#define CONVERSION 1.609;
+    double miles;
+    double kilometers;
+    
+    cout << "Please enter the number of miles you drive to work: ";
+    cin >> miles;
+    
+    kilometers = miles * CONVERSION;
+    
+    cout << "You drive " << kilometers << " km to work.\n";
 }
